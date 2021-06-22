b0ddec1c2c2ef57e91d9495e75f4f0ad8888aa22
anas <civil.anas1000@gmail.com>
2021-06-22 19:58:44 +0300

    fix everything

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT;
 const app = express();
+app.use(express.json());
 const mongoose = require('mongoose');
 const MONGODB=process.env.MONGODB_URI
+
 app.use(cors());
 
 const {
@@ -14,13 +16,13 @@ const {
     handleDeleteBook,
     homeHandler,
 } = require('./moduls/seedCollection');
-
-mongoose.connect('mongodb://localhost:27017/myfavariteBook', {useNewUrlParser: true, useUnifiedTopology: true});
-// mongoose.connect(MONGODB, {useNewUrlParser: true, useUnifiedTopology: true});
-
-
 app.get('/',homeHandler);
 app.get('/book',bookHandler)
+app.post('/addbook', handleAddBook);
+app.delete('/deletebook/:index', handleDeleteBook);
+
+// mongoose.connect('mongodb://localhost:27017/myfavariteBook', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(MONGODB, {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 app.listen(PORT, () => {
